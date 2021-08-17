b82df593f9760682693a7de3225536a1f19c3ea9
DaveAldon <crawford@mail.gvsu.edu>
2021-08-17 16:54:31 -0400

    Added more resource screenshots

--- src/commands/windowCommand.ts
+++ src/commands/windowCommand.ts
@@ -14,5 +14,5 @@ export class WindowCommand implements Command {
 }
 
 export const testable = () => {
-    return `https://github.com/DaveAldon/bravo-discord-bot/blob/main/repoResources/onlineScreenshot.png`
+    return `https://raw.githubusercontent.com/DaveAldon/bravo-discord-bot/main/repoResources/street.jpg`
 }
