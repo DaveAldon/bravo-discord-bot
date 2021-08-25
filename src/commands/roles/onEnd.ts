0f671f916e849ed9619a150aa2e8037c6c0ae368
DaveAldon <crawford@mail.gvsu.edu>
2021-08-25 10:07:54 -0400

    Updated wording on embed end

--- src/commands/roles/onEnd.ts
+++ src/commands/roles/onEnd.ts
@@ -4,7 +4,7 @@ export const onEnd = (message: Message, _collected: { size: number; }) => {
     const commandString = "`!roles`"
     const removeCommandString = "`!roles remove`"
     const outputEmbed = new Discord.MessageEmbed()
-        .setTitle("**Role Removal Ended**")
+        .setTitle("**Role Selection Ended**")
         .setDescription(`If you'd like to add a skill based role to your account, run the ${commandString} command and follow the instructions.
             \nIf you'd like to remove a skill based role from your account, run the ${removeCommandString} command and follow the instructions.`)
     message.channel.send(outputEmbed);
