9085fd7ae0c1bb3e7f18fde99e02dd9aa14c6e58
DaveAldon <crawford@mail.gvsu.edu>
2021-09-04 16:25:46 -0400

    Updated client id

--- src/config/secrets.ts
+++ src/config/secrets.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env' });
-export const CLIENT_ID = '719659727679651860';
+export const CLIENT_ID = '876206083461505025';
 export const DISCORD_TOKEN = process.env['token'];
 
 if (!DISCORD_TOKEN) {
