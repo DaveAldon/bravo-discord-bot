2de22d2af4f3aad1def6c28685a6d7f68e6ad181
David Crawford <DaveAldon@users.noreply.github.com>
2021-12-27 15:37:52 -0500

    Added handling for 2k char limit

--- src/commands/jobs.ts
+++ src/commands/jobs.ts
@@ -17,7 +17,18 @@ export const jobs = () => {
       try {
         props.filter = `${interaction.options.data[0].value}`;
       } catch (e) {}
-      await interaction.reply(await getJobs(props));
+      // split up the message to bypass the 2k char limit per message
+      const messageArray = await getJobs(props)
+      if(messageArray.length % 2 > 0) messageArray.push("")
+      const half = Math.floor(messageArray.length / 2);
+      if(messageArray.length > 1) {
+      const firstHalf = messageArray.slice(0, half).join(" ")
+      const secondHalf = messageArray.slice(-half).join(" ")
+      await interaction.reply(firstHalf);
+      interaction.channel?.send(secondHalf)
+      } else {
+        await interaction.reply(messageArray.join(" "));
+      }
     },
   };
 };
@@ -58,7 +69,7 @@ const getJobs = async (props: IGetJobs) => {
         ` **${filter}**`) ||
         ''} position${returnMessage.length === 2 ? '' : 's'} right now!\n\n`;
     }
-    return returnMessage.join('');
+    return returnMessage;
   };
   return result();
 };
